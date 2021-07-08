73bee023ccb5062f662cb8cc41365bf2ab96a162
champix <desorbaix@free.fr>
2021-07-08 23:43:36 +0200

    RGU add and implem

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import './App.css';
 import Button from './components/Button/Button';
+import ValuesViewer from './components/ValuesViewer/ValuesViewer';
+import * as RGU from 'react-graphical-ui';
+class App extends React.Component {
+  state:{values : number[]}
+  constructor(props:any){
+    super(props);
+    this.state={values:[20,30,52,54,85,12,42,41]};
+  }
+  render() {
+    return (
+      <div className="App">
+        <ValuesViewer values={this.state.values}></ValuesViewer>
+        <Button text="colored button" bgcolor="tomato" />
+        <Button text="colored button" bgcolor="skyblue" />
+        <hr/>
+      <RGU.Histogram values={this.state.values} max={100} notAllreadyFull={false}  gridDivisionCount={10} display="inline" />
+      <RGU.CloudPoints values={this.state.values} max={100} notAllreadyFull={false}  gridDivisionCount={10} display="inline" />
 
-function App() {
-  return (
-    <div className="App">
-     <Button text="colored button" bgcolor="tomato" />
-     <Button text="colored button" bgcolor="skyblue" />
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
